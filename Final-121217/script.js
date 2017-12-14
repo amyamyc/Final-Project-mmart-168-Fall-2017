dd5276137d52c2c0cbd88884fe12d7dc001c8ccc
amyamyc <31627281+amyamyc@users.noreply.github.com>
2017-12-14 13:16:27 -0800

    Final 12-14-17. Version to be graded.

--- Final-121217/script.js
+++ Final-121217/script.js
@@ -3,48 +3,33 @@ const sheets = new Sheets({
     //query: "select * where D = 'C'"
 })
 
-const bardata = []
+//let variable shifts in value
+let bardata = []
 
-sheets.getData(data => {
-    //loop through records and output to the screen:
-    console.log(data)
-    let num = 1
-    data.forEach(record => {
-        if (record.code === 'P') {
-            bardata.push(100)
-        } else if (record.code === 'US') {
-                bardata.push(80)
-
-        } else {
-            bardata.push(50)
-        }
-    })
-
-    buildChart()
-
-})
-
-const buildChart = () => {
-
-//const bardata = [100,100, 30, 30, 30, 100, 100, 100, 30, 30, 30, 100];
-
-const  margin = {top: 30, right: 30, bottom: 40, left: 50 }
-
-// Size of the box that the chart sits in
+const  margin = {top: 30, right: 30, bottom: 40, left: 30 }
 const  height = 900 - margin.top - margin.bottom,
     width = 1000 - margin.left- margin.right,
     barWidth = 10,
     barOffset = 5
 
-//----------------- Y Vertical Axis --------------------- //
-
 //Linear scale for numbers in order.
-const  yScale = d3.scale.linear()
-// Y axis from number zero through the max barData value. Domain represents the
-// boundaries within your data.
-    .domain([0, d3.max(bardata)])
-//Y axis from 0 to the height noted above.
+const yScale = d3.scale.linear()
+    // Y axis from number zero through the max barData value. Domain represents the
+    // boundaries within your data.
+    .domain([0, 100])
+    //Y axis from 0 to the height noted above.
     .range([0, height])
+
+    sheets.getData(data => {
+        //loop through records and output to the screen:
+        bardata = data
+        console.log(data)
+        buildChart()
+
+    })
+
+
+const buildChart = () => {
 
 //------------------- X Horizontal Axis ------------------- //
 
@@ -69,8 +54,23 @@ const  tooltip = d3.select('body').append('div')
     .style('background', 'white')
     .style('opacity', 0)
 
-//--------------------The Chart Area ---------------------- //
 
+//------------------------Labeling the X Axis  ---------------------------- //
+
+/* Some starting text to begin label the X axis. Need to define svg or d3.select
+//the element before using it.
+svg.append('text')
+    .attr('class', 'x label')
+    .attr('text-anchor', 'end')
+    .attr('x', width)
+    .attr('y', height - 6)
+    .text('income per capita, inflation-adjusted (dollars)')*/
+
+
+//------------------------The Chart Area ------------------------------- //
+
+
+// Select id chart and appending an svg to it.
 const  myChart = d3.select('#chart').append('svg')
   // Add background color of the box that the chart area sits in.
   .style('background', '#E7E0CB')
@@ -87,21 +87,13 @@ const  myChart = d3.select('#chart').append('svg')
         })
         .attr('width', xScale.rangeBand())
         .attr('height', 0)
-          //.attr('y', height)
-          //.attr('x', function(d,i) {
-          //return xScale(i);
-          //})
 
         //'d' passing along data and 'i' index of the data array. Creating space
         // between each vertical bar.
         .attr('x', function(d,i){
           return i * (barWidth + barOffset + .5)
         })
-        // To position the vertical bars to the bottom of the chart area
-        //instead of the top of the chart box.
-        .attr('y', function(d){
-          return height-yScale(d)
-        })
+
 
 //-----------------The Mouse Over Interaction with Tool Tip ---------------//
 
@@ -109,21 +101,29 @@ const  myChart = d3.select('#chart').append('svg')
     console.log(d)
       tooltip.transition()
         .style('opacity', .9)
-        // Placing barData into the tooltip box
-      //  tooltip.html(d)
-      //tooltip.html(d)
-          // Adjusting the position of the tool tip
-          .style('left', (d3.event.pageX -30 ) + 'px')
-          .style('top', (d3.event.pageY- 35) + 'px')
+        // Adjusting the position of the tool tip
+        .style('left', (d3.event.pageX -30 ) + 'px')
+        .style('top', (d3.event.pageY- 35) + 'px')
+        // Placing Year and Event from Sheets (Column A and B) into the tooltip box
+        tooltip.html(
+            '<span style="color:red"><h1>' + d.year + '</h1>' +
+            '<p>' + d.category + '</p>' +
+            '<p>' + d.event + '</p>' +
+            '<p> Location: ' + d.location + '</p>'
 
-        //record.event to isolate Event??? Some solutions that do not work..
-        //d["record.event"]???
-        //tooltip.html(d['year'])???
-        //tooltip.html(d[record.A])???
-        //Query??
+        )
 
-        //If want to include picture and description in tool tip box
-        tooltip.html("<span style='color:red'><h1>Tombouctou Region</h1><br><ahref='http://en.wikipedia.org/wiki/Tombouctou_Region'><img src='http://upload.wikimedia.org/wikipedia/commons/b/b3/Mali_Tombouctou.png'></ahref></span> <span style='color:blue'><h2>Mali !!!!!!! </h2></span>")
+//-----------Writing a conditional to render images in the tooltip----------//
+          /*if (d.link exists) {
+          return the d.link img src link in the tool tip
+        }*/
+//---------------------Ternery Operators-----------------------------------//
+          //condition ? expr1 : expr2
+          //This means if condition is true then return expr 1 value. Otherwise, return value expr2.
+
+
+//If want to include picture and description in tool tip box
+//tooltip.html("<span style='color:red'><h1>Tombouctou Region</h1><br><ahref='http://en.wikipedia.org/wiki/Tombouctou_Region'><img src='http://upload.wikimedia.org/wikipedia/commons/b/b3/Mali_Tombouctou.png'></ahref></span> <span style='color:blue'><h2>Mali !!!!!!! </h2></span>")
 
         tempColor = this.style.fill
             d3.select(this)
@@ -137,19 +137,15 @@ const  myChart = d3.select('#chart').append('svg')
                 .style('fill', tempColor)
         })
 
+//Animation of the chart. Bringing the chart contents up once page is refreshed.
  myChart.transition()
-    .attr('height', function(d){
-        return yScale(d)
-    })
-    .attr('y', function(d) {
-        return height - yScale(d)
-    })
-    /*.delay(function (d, i) {
-        return i * 20;
-    })
-    .duration(1000)
-    .ease('elastic')*/
-
+   .attr('height', getHeight)
+   .attr('y', getY)
+   .delay(function(d, i) {
+       return i * 50
+   })
+   .duration(1000)
+   .ease('elastic')
 
 //---------------------- Y Axis Guide ----------------------------//
 
@@ -157,33 +153,37 @@ var vGuideScale = d3.scale.linear ()
 	.domain([0, d3.max(bardata)])
 	.range([height, 0])
 
-
-var vAxis = d3.svg.axis()
+/*var vAxis = d3.svg.axis()
 	.scale(vGuideScale)
 	.orient('left')
-	.ticks(10)
+	.ticks(10)*/
 
-var vGuide = d3.select('svg').append('g')
+ var vGuide = d3.select('svg').append('g')
 	vAxis(vGuide)
 
 	vGuide.attr('transform', 'translate('+ margin.left +', '+ margin.top +')')
 	vGuide.selectAll('path')
-		.style({ fill: 'none', stroke: '#000'})
+		  .style({ fill: 'none', stroke: '#000'})
 	vGuide.selectAll('line')
 		.style({stroke: '#000'})
 }
 
-/*var hAxis = d3.svg.axis()
-	.scale(xScale)
-	.orient('bottom')
-	.tickValues(xScale.domain().filter(function(d,i){
-		return !(i % (bardata.length/5));
-	}))*/
+// Assigning height values to code P, US and all else to 50.
+const getHeight = function (d) {
+    let y = 50
+    if (d.code === 'P') {
+        y = 100
+    } else if (d.code === 'US') {
+        y = 80
+    }
 
-/* var hGuide= d3.select('svg').append('g')
-	hAxis(hGuide)
-	hGuide.attr('transform', 'translate('+ margin.left +', '+ (height+ margin.top) +')')
-	hGuide.selectAll('path')
-		.style({ fill: 'none', stroke: "#000"})
-	hGuide.selectAll('line')
-		.style({stroke: "#000"})*/
+    const h =  yScale(y)
+    console.log('Height:', h, y, d.code)
+    return h
+}
+
+const getY = function (d) {
+    const y = height - getHeight(d)
+    console.log('y:', y)
+    return y
+}
